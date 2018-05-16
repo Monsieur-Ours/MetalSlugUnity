06a0093d3133934909a61648768f7f43d2b6573f
Alexis Morvan <alexis.morvan@outlook.com>
2018-05-16 10:16:41 +0200

    Added grenade for enemy and player and health bar for the player

---
+++ Assets/Scripts/HealthController.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthController : MonoBehaviour {
+
+    public string effectorTag;
+    public float maxHealth;
+
+    protected float currentHealth;
+    protected SpriteRenderer sprite;
+
+    private void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    private void Awake()
+    {
+        sprite = GetComponent<SpriteRenderer>();
+    }
+
+    private void TakeDamageEffect( DamageEffector effector)
+    {
+
+        Debug.Log(effector);
+        if (effector.CompareTag(effectorTag))
+        {
+            TookDamage(effector.damage);
+        }
+    }
+
+    virtual public void TookDamage(int damage)
+    {
+        currentHealth -= damage;
+        if (currentHealth <= 0)
+        {
+            gameObject.SendMessage("Dead", currentHealth, SendMessageOptions.DontRequireReceiver);
+        }
+        else
+        {
+            StartCoroutine(TookDamageCoroutine());
+        }
+    }
+
+    virtual public void GainHealth(int gain)
+    {
+        if (currentHealth < maxHealth)
+        {
+            currentHealth += gain;
+        }
+        else
+        {
+            currentHealth = maxHealth;
+        }
+
+    }
+
+    IEnumerator TookDamageCoroutine()
+    {
+        sprite.color = Color.red;
+        yield return new WaitForSeconds(0.1f);
+        sprite.color = Color.white;
+    }
+
+}
---
+++ Assets/Scripts/PayerHealthController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class PayerHealthController : HealthController {
+
+    public Slider healthBar;
+
+	// Use this for initialization
+	void Start () {
+        currentHealth = maxHealth;
+        healthBar.value = CalculatedHealth();
+	}
+
+    public override void TookDamage(int damage)
+    {
+        base.TookDamage(damage);
+
+        healthBar.value = CalculatedHealth();
+    }
+
+    public override void GainHealth(int gain)
+    {
+        base.GainHealth(gain);
+
+        healthBar.value = CalculatedHealth();
+    }
+
+    float CalculatedHealth()
+    {
+        return currentHealth / maxHealth;
+    }
+}
